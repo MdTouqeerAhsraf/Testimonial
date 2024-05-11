1a10b003a3d152b88e8ea774d57a2ad1ef6eae50
Md Touqeer Ashraf <141518098+MdTouqeerAhsraf@users.noreply.github.com>
2024-05-11 23:37:16 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const reviews = [
+  {
+    id: 1,
+    name: "MD Touqeer Ashraf",
+    job: "Software Engineer",
+    image: "https://i.pinimg.com/564x/8e/bb/93/8ebb93e537fed1af81f34a603a6f748e.jpg",
+    text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solutions on time and within budget. They are a true partner and an asset to any project.",
+  },
+  {
+    id: 2,
+    name: "Md Farhan Akhtar",
+    job: "Graphic Designer",
+    image: "https://i.pinimg.com/564x/8c/b4/74/8cb474a2519baa3f52880f4ad762cb5c.jpg",
+    text: "I have been working with this company for several years now, and I have always been impressed with their creativity and attention to detail. They are true professionals who take pride in their work and always go above and beyond to deliver exceptional results.",
+  },
+  {
+    id: 3,
+    name: "Md Asif Ansari",
+    job: "Marketing Manager",
+    image: "https://i.pinimg.com/564x/ea/14/b9/ea14b9736d5a737501b7991f058908d8.jpg",
+    text: "I am thrilled with the results of our recent marketing campaign, and it wouldn't have been possible without the hard work and dedication of the entire team. Thank you for your exceptional work!",
+  },
+  {
+    id: 4,
+    name: "Md Aman ",
+    job: "Content Strategist",
+    image: "https://i.pinimg.com/564x/20/d8/aa/20d8aa124311d3d0e05fa56d68ee502f.jpg",
+    text: "I have worked with many content creators over the years, but none have impressed me as much as this team. They have a knack for crafting compelling and engaging content that resonates with our audience and drives real results. I highly recommend them!",
+  },
+  {
+    id: 5,
+    name: "Parween",
+    job: "Data Analyst",
+    image: "https://i.pinimg.com/564x/a2/56/19/a256193b83439d0cdab919f15e38010c.jpg",
+    text: "I had the opportunity to work with this team on a data analysis project, and I was impressed with their attention to detail and their ability to work with complex datasets. They provided valuable insights that helped us make informed business decisions. It was a pleasure working with them.",
+  },
+];
+
+export default reviews;
